ed17422eab01ae53e8aa4a37b5e318d273a9fa59
kuan <apkapb@gmail.com>
2022-04-26 21:08:26 +0800

    fix: vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import legacy from '@vitejs/plugin-legacy';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
@@ -12,8 +13,15 @@ export default defineConfig({
     alias: { '@': resolve('src') },
     extensions: ['.tsx', '.js', '.ts'],
   },
+  build: {
+    minify: 'terser',
+    terserOptions: { compress: { drop_debugger: true, drop_console: true } },
+  },
   plugins: [
     react(),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
     UnoCSS({
       // 但 `presets` 被指定时，默认的预设将会被禁用，
       // 因此你可以在你原有的 App 上使用纯 CSS 图标而不需要担心 CSS 冲突的问题。
