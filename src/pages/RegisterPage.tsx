d792f33e18597c82e37ab13fe7f909e107bcb96f
tim <tim@tim.com>
2022-04-27 11:37:46 +0800

    feat: add register page

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,18 +1,18 @@
 import React, { FC } from 'react';
 
-import noAccess from '@/assets/illustrations/no-access.svg';
+import wait from '@/assets/illustrations/wait.svg';
 import styles from './styles/login.module.scss';
 import { Button, Input } from '@douyinfe/semi-ui';
-type ILoginPage = {};
-const LoginPage: FC<ILoginPage> = (props) => {
-  console.log('loginPage render...');
+type IRegisterPage = {};
+const RegisterPage: FC<IRegisterPage> = (props) => {
+  console.log('registerPage render...');
   return (
     <div className={`dark:bg-dark h-screen w-screen flex items-center justify-center`}>
       <div className={styles.loginWrapper}>
-        <img className={styles.img} src={noAccess} alt="waiting" />
+        <img className={styles.img} src={wait} alt="waiting" />
         <div className={styles.login}>
           <h3 className={`font-bold dark:text-white text-2xl mb-5 text-center`}>
-            欢迎登录
+            欢迎注册
           </h3>
           <div className={styles.form}>
             <Input
@@ -31,13 +31,13 @@ const LoginPage: FC<ILoginPage> = (props) => {
               size={`large`}
             />
             <Button type="primary" size="large" theme={`solid`} block>
-              登录
+              注册
             </Button>
 
             <p className={`text-sm dark:text-white`}>
-              还没有账号？
-              <a href="#/register" className={`text-blue-600`}>
-                点击注册
+              已经有账号？
+              <a href="#/login" className={`text-blue-600`}>
+                点击登录
               </a>
               <a href="#" className={`text-blue-600 ml-3`}>
                 返回首页
@@ -50,4 +50,4 @@ const LoginPage: FC<ILoginPage> = (props) => {
   );
 };
 
-export default LoginPage;
+export default RegisterPage;
