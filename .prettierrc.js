1cd577b801fee40c65c666284011269b3fec5eca
kuan <apkapb@gmail.com>
2023-06-03 09:41:43 +0800

    fix: update vite config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-    semi: true,
-    trailingComma: 'all',
-    singleQuote: true,
-    printWidth: 90,
-    tabWidth: 2,
-    jsxBracketSameLine: true,
-    endOfLine: 'auto',
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 90,
+  tabWidth: 2,
+  jsxBracketSameLine: true,
+  endOfLine: 'auto',
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import vitePluginForArco from '@arco-plugins/vite-react';
 import legacy from '@vitejs/plugin-legacy';
-import react from "@vitejs/plugin-react-swc";
+import react from '@vitejs/plugin-react-swc';
 import * as path from 'path';
 import UnoCSS from 'unocss/vite';
 import { defineConfig } from 'vite';
@@ -19,17 +19,17 @@ export default defineConfig({
   },
   build: {
     minify: 'esbuild',
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: { react: 'React', 'react-dom': 'ReactDOM' },
-        paths: {
-          react: 'https://esm.sh/react@17',
-          'react-dom': 'https://esm.sh/react-dom@17',
-          // react: 'https://g.alicdn.com/code/lib/react/17.0.2/umd/react.production.min.js',
-        },
-      },
-    },
+    // rollupOptions: {
+    //   external: ['react', 'react-dom'],
+    //   output: {
+    //     globals: { react: 'React', 'react-dom': 'ReactDOM' },
+    //     paths: {
+    //       react: 'https://esm.sh/react@17',
+    //       'react-dom': 'https://esm.sh/react-dom@17',
+    //       // react: 'https://g.alicdn.com/code/lib/react/17.0.2/umd/react.production.min.js',
+    //     },
+    //   },
+    // },
     // terserOptions: { compress: { drop_debugger: true, drop_console: true } },
   },
   esbuild: {
